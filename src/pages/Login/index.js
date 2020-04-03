fc0f58092207a353da849a9cd066dfded62e2b05
Jaroslav Janda <janda.jaroslav5@gmail.com>
2020-04-03 21:37:05 +0200

    login page fix

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,13 +1,14 @@
 import React, { useEffect } from 'react'
 
 const LogIn = ({ location, auth }) => {
-  return useEffect(() => {
+  useEffect(() => {
     if (/acess_token|id_token|error/.test(location.hash)) {
       auth.handleAuthetication()
     } else {
       throw new Error('Invalid callback URL')
     }
   })
+  return <div />
 }
 
 export default LogIn
